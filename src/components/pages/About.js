bd14e4b08ae7624fd90b28f4c5d794144c91014e
veronicawonghy <veronicawonghy@gmail.com>
2021-02-09 19:59:44 +0800

    about pic fix

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,10 +8,14 @@ function About() {
         
         <Container className='mainContainer'>
             <Row>
-                <Col xs={12} md={5}><img src={myPicture} alt="My Picture" className='myPicture contentContainer' /></Col>
+                <Col xs={12} md={5}>
+                    <div className='d-flex justify-content-center'>
+                        <img src={myPicture} alt="My Picture" className='myPicture contentContainer' />
+                    </div>
+                </Col>
                 <Col xs={12} md={7} className='contentContainer'>
                 <div>
-                    <p>Hello, I am Veronica! </p>
+                    <p><h3>Hello, I am Veronica!</h3></p>
                     
                     <p>I am currently a final year student at Hong Kong University of Science and Technology, 
                     majoring in Computer Engineering and minoring in Business.</p>
@@ -19,15 +23,16 @@ function About() {
                     <p>I have always been interested in technology since I was a child. I believe that technology could create a great positive impact to our society. 
                     I found programming stimulating as it is always challenging and I feel satisfied whenever I complete a program. It also makes me a more logical and analytical person. :) </p>
                     
-                    <p>During the course of my academic career, I also gained work experience in several industries, like Banking, Telecom, Tech start-up etc.
-                    I have acquired skills in programming like Python, SQL, Javascript, C++ and Java. I am also happy to learn new technical stack.</p>
+                    <p>During the course of my academic career, I also gained working experience in several industries, like Banking, Telecom, Tech start-up etc.
+                    I have acquired skills in programming like Python, SQL, Javascript, C++ and Java. I also enjoy learning new technical stack.</p>
 
-                    <p>Personally, I like to do yoga and learn languages. I am learning Korean and French recently! :D</p>
+                    <p>Personally, I like to do yoga, watch Netflix and learn languages. I am learning Korean and French recently! :D</p>
                     
                     <p>Connect me via:
-                    LinkedIn
-                    Instagram
-                    Email
+                        <br/>
+                        LinkedIn
+                        Instagram
+                        Email
                     </p>
                 </div>
                 </Col>
